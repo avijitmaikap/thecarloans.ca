eadeba761a6ae6940d3c4077f8f03e239927a2ea
srmarohit <srmarohit@gmail.com>
2021-11-07 12:14:50 +0530

    Format ready

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <>
+        <button className="btn btn-success">heelo</button>
+       </>
+    )
+}
+
+export default NavBar

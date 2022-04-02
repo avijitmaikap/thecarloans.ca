8f3e3de992142d5a8cc4664d80eb4d1779f8d742
Avijit <maikapavijit@gmail.com>
2022-04-02 13:14:50 -0400

    About us rectifie fixed

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -4,7 +4,7 @@ import "../styles/aboutUs.scss"
 
 function AboutUs() {
     return (
-        <div className = "container-fluid mt-5 pt-5">
+        <div className = "container-fluid mt-5 py-5">
             <SectionTitle title="About Us"/>
             <div className = "container aboutus">
                 <p>We feel that a vehicle loan has the potential to be absolutely miraculous in terms of improving your lifestyle, growing your wealth, and providing financial prospects.</p>
@@ -19,9 +19,11 @@ Most significantly, we provide you with the opportunity to repair and reestablis
 Our staff is a committed collection of skilled, professional financial advisers, sales professionals, and vehicle enthusiasts, but most importantly - individuals just like you who have experienced life's ups and downs — we genuinely understand.
 
 We appreciate every chance to better the lives of our neighbours.</p>
+
+<img className = "cover-img" src="https://images.unsplash.com/photo-1541899481282-d53bffe3c35d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" alt=""/>
+
 </div>
 
-<img src="https://assets.carpages.ca/dealersite/prod-wp-approval-genie/uploads/2019/06/landing-page-autos-full.png" alt=""/>
         </div>
         
     )
